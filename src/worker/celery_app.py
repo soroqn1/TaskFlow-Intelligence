efe211c8f7396b784076cd434c49703f7ec21180
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-02-03 15:40:29 +0200

    celery worker setup

---
+++ src/worker/celery_app.py
@@ -0,0 +1,12 @@
+import os
+from celery import Celery
+
+celery_app = Celery(
+    "worker",
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0"
+)
+
+celery_app.conf.update(
+    task_track_started=True
+)
